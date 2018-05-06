56dc7bc1abc90cd7f3cc2a5c107ad24a5a1fb091
Michael Markl <marklmichael98@gmail.com>
2018-05-06 20:00:22 +0200

    NEUBURG-14: Remove splashscreen and service worker stuff

--- src/main.js
+++ src/main.js
@@ -9,16 +9,6 @@ const container = document.getElementById('container')
 
 ReactDOM.render(<App />, container)
 
-// Sets the splash to hidden when the page is rendered
-document.getElementById('splash').className += ' splash-hidden'
-
-// Currently we do not have service workers. Unregister all previous ones:
-if (navigator.serviceWorker) {
-  navigator.serviceWorker.getRegistrations().then(registrations => {
-    registrations.forEach(registration => { registration.unregister() })
-  })
-}
-
 // Enables hot-module-reloading if it's enabled
 if (module.hot) {
   module.hot.accept()
