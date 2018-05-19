0cbc040d28180b3595d86ab9bd56e80f68a91579
Michael Markl <marklmichael98@gmail.com>
2018-05-19 13:18:26 +0200

    NEUBURG-17: Update hostname for neuburgschrobenhausen

--- src/modules/city-detection/cityConfigs.js
+++ src/modules/city-detection/cityConfigs.js
@@ -4,7 +4,7 @@ import logoNeuburg from './assets/neuburg-project.svg'
 import logoBayreuth from './assets/bayreuth.png'
 
 const configs = [
-  new CityConfig('neuburgschrobenhausenwohnraum', 'neuburg.wohnen.integreat-app.de', true, 'Neuburg-Schrobenhausen', logoNeuburg),
+  new CityConfig('neuburgschrobenhausenwohnraum', 'raumfrei.neuburg-schrobenhausen.de', true, 'Neuburg-Schrobenhausen', logoNeuburg),
   new CityConfig('bayreuthwohnraum', 'bayreuth.wohnen.integreat-app.de', false, 'Bayreuth', logoBayreuth)
 ]
 
