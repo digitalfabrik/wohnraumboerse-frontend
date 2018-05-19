545bda805b6e7ead51e271015b29ed38007350e3
Michael Markl <marklmichael98@gmail.com>
2018-05-19 13:33:07 +0200

    NEUBURG-17: Remove impressumUrl from cityConfig model

--- src/modules/city-detection/CityConfig.js
+++ src/modules/city-detection/CityConfig.js
@@ -1,11 +1,10 @@
 class CityConfig {
-  constructor (cmsName, hostname, formsEnabled, title, logo, impressumUrl) {
+  constructor (cmsName, hostname, formsEnabled, title, logo) {
     this.cmsName = cmsName
     this.hostName = hostname
     this.formsEnabled = formsEnabled
     this.title = title
     this.logo = logo
-    this.impressumUrl = impressumUrl
   }
 }
 
