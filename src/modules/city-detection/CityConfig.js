e6b6ed52d5cc318422bc0f00b313718dfd101fc8
Michael Markl <marklmichael98@gmail.com>
2018-05-19 13:33:17 +0200

    NEUBURG-17: Remove title from cityConfig model

--- src/modules/city-detection/CityConfig.js
+++ src/modules/city-detection/CityConfig.js
@@ -1,9 +1,8 @@
 class CityConfig {
-  constructor (cmsName, hostname, formsEnabled, title, logo) {
+  constructor (cmsName, hostname, formsEnabled, logo) {
     this.cmsName = cmsName
     this.hostName = hostname
     this.formsEnabled = formsEnabled
-    this.title = title
     this.logo = logo
   }
 }
