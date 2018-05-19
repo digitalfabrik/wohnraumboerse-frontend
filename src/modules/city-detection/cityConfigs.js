312a3015487b8b01b42b436a47ce3f7f3459a347
Michael Markl <marklmichael98@gmail.com>
2018-05-19 13:34:28 +0200

    NEUBURG-17: Remove title from cityConfig models

--- src/modules/city-detection/cityConfigs.js
+++ src/modules/city-detection/cityConfigs.js
@@ -4,8 +4,8 @@ import logoNeuburg from './assets/neuburg-project.svg'
 import logoBayreuth from './assets/bayreuth.png'
 
 const configs = [
-  new CityConfig('neuburgschrobenhausenwohnraum', 'raumfrei.neuburg-schrobenhausen.de', true, 'Neuburg-Schrobenhausen', logoNeuburg),
-  new CityConfig('bayreuthwohnraum', 'bayreuth.wohnen.integreat-app.de', false, 'Bayreuth', logoBayreuth)
+  new CityConfig('neuburgschrobenhausenwohnraum', 'raumfrei.neuburg-schrobenhausen.de', true, logoNeuburg),
+  new CityConfig('bayreuthwohnraum', 'bayreuth.wohnen.integreat-app.de', false, logoBayreuth)
 ]
 
 export default configs
