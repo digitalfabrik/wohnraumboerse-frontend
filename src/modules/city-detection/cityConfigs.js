01910326ce2f90cf3ae2588c40cd8f33592709fb
Michael Markl <marklmichael98@gmail.com>
2018-08-06 20:30:56 +1000

    NEUBURG-100: Add testumgebung cityConfig

--- src/modules/city-detection/cityConfigs.js
+++ src/modules/city-detection/cityConfigs.js
@@ -1,12 +1,15 @@
 import CityConfig from './CityConfig'
 
-import logoNeuburg from './assets/neuburg_logo.svg'
-import logoBayreuth from './assets/bayreuth_logo.png'
-import faviconBayreuth from './assets/bayreuth_favicon.ico'
 import faviconNeuburg from './assets/neuburg_favicon.ico'
+import logoNeuburg from './assets/neuburg_logo.svg'
+import faviconBayreuth from './assets/bayreuth_favicon.ico'
+import logoBayreuth from './assets/bayreuth_logo.png'
+import faviconTestumgebung from './assets/testumgebung_favicon.ico'
+import logoTestumgebung from './assets/testumgebung_logo.png'
 
 const configs = [
   new CityConfig('neuburgschrobenhausenwohnraum', 'raumfrei.neuburg-schrobenhausen.de', true, 'Neuburg-Schrobenhausen', logoNeuburg, faviconNeuburg),
+  new CityConfig('testumgebungwohnraum', 'test.wohnen.integreat-app.de', true, 'Testumgebung', logoTestumgebung, faviconTestumgebung),
   new CityConfig('bayreuthwohnraum', 'wohnraumboerse.bayreuth.de', false, 'Bayreuth', logoBayreuth, faviconBayreuth)
 ]
 
