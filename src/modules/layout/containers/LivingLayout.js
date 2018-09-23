087af9cc47fa13510cbe5297ea572795f7b1ff3c
Nikolas Klug <klug.nikolas@googlemail.com>
2018-09-23 16:12:56 +0200

    NEUBURG-27: Add endpoint to LivingLayout

--- src/modules/layout/containers/LivingLayout.js
+++ src/modules/layout/containers/LivingLayout.js
@@ -1,26 +1,28 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
+import { connect, compose } from 'react-redux'
 
+import withFetcher from 'modules/endpoint/hocs/withFetcher'
 import Layout from '../components/Layout'
 import LivingHeader from '../components/LivingHeader'
 import LivingFooter from '../components/LivingFooter'
+import CityConfig from '../../city-detection/CityConfig'
 import getCurrentCityConfig from '../../city-detection/getCurrentCityConfig'
-
-const cityConfig = getCurrentCityConfig()
 
 export class LivingLayout extends React.Component {
   static propTypes = {
     matchRoute: PropTypes.func.isRequired,
     currentPath: PropTypes.string.isRequired,
     viewportSmall: PropTypes.bool.isRequired,
-    children: PropTypes.node
+    children: PropTypes.node,
+    cityConfig: PropTypes.arrayOf(CityConfig).isRequired
   }
 
   render () {
     const {matchRoute} = this.props
     return <Layout header={<LivingHeader viewportSmall={this.props.viewportSmall}
-                                         matchRoute={matchRoute} location={cityConfig.cmsName}
+                                         matchRoute={matchRoute}
+                                         location={getCurrentCityConfig(this.props.cityConfig.cmsName)}
                                          currentPath={this.props.currentPath} />}
                    footer={<LivingFooter matchRoute={matchRoute} />}>
       {this.props.children}
@@ -33,4 +35,7 @@ const mapStateToProps = state => ({
   viewportSmall: state.viewport.is.small
 })
 
-export default connect(mapStateToProps)(LivingLayout)
+export default compose(
+  connect(mapStateToProps),
+  withFetcher('cityConfigs')
+)(LivingLayout)
