8540b680ec15d76882bfcf15f5de569d54c373ac
Nikolas Klug <klug.nikolas@googlemail.com>
2018-10-06 11:42:52 +0200

    NEUBURG-27: Add correct logo url

--- src/modules/layout/components/Header.js
+++ src/modules/layout/components/Header.js
@@ -37,7 +37,7 @@ class Header extends React.Component {
         <header className={style.header}>
           <div className={style.logoWide}>
             <Link href={this.props.logoHref}>
-              <img src={`${environment.apiBaseUrl}city-configs/image/${this.props.logo}`} />
+              <img src={this.props.logo} />
             </Link>
           </div>
           <HeaderActionBar className={style.actionBar} items={this.props.actionItems} />
