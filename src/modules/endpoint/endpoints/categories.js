c2d65b13b899c799b1e05429a5b7d1fdf88652c6
Nikolas Klug <klug.nikolas@googlemail.com>
2018-10-16 16:47:28 +0200

    NEUBURG-20: Use api v3 in categories endpoint

--- src/modules/endpoint/endpoints/categories.js
+++ src/modules/endpoint/endpoints/categories.js
@@ -1,51 +1,41 @@
 import EndpointBuilder from '../EndpointBuilder'
 
-import normalizeUrl from 'normalize-url'
 import CategoryModel from '../models/CategoryModel'
 import CategoriesMapModel from '../models/CategoriesMapModel'
 import getCurrentCityConfig from '../../city-detection/getCurrentCityConfig'
+import moment from 'moment'
+import { compose, toPairs } from 'lodash'
+import normalizePath from 'normalize-path'
 
-function getUrlWithoutCity (url) {
-  const tempUrl = url.split('/')
-  tempUrl.shift()
+const PREFIX_LENGTH = 3
+
+// Removes the (unnecessary) path prefix
+function getPathWithoutPrefix (url) {
+  const tempUrl = url.split('/').slice(PREFIX_LENGTH)
   return tempUrl.join('/')
 }
 
 export default new EndpointBuilder('categories')
   .withStateToUrlMapper(state => {
     return `https://cms.integreat-app.de/${getCurrentCityConfig(state.cityConfigs._data).cmsName}` +
-      `/de/wp-json/extensions/v0/modified_content/pages?since=1970-01-01T00:00:00Z`
+      `/de/wp-json/extensions/v3/pages`
   })
   .withMapper((json, state) => {
-    const baseUrl = ``
-    const categories = json.filter(category => category.status === 'publish')
-      .map(category => {
-        return new CategoryModel({
-          id: category.id,
-          url: normalizeUrl(`${baseUrl}/${getUrlWithoutCity(category.permalink.url_page)}`),
-          title: category.title,
-          parentId: category.parent,
-          content: category.content,
-          thumbnail: category.thumbnail,
-          order: category.order
-        })
+    const normalize = compose([decodeURIComponent, normalizePath])
+    const basePath = ``
+    const categories = json.map(category => {
+      return new CategoryModel({
+        id: category.id,
+        path: normalize(getPathWithoutPrefix(category.path)),
+        title: category.title,
+        content: category.content,
+        thumbnail: category.thumbnail,
+        order: category.order,
+        availableLanguages: new Map(toPairs(category.available_languages)
+          .map(([key, value]) => [key, normalize(value.path)])),
+        parentPath: normalize(getPathWithoutPrefix(category.parent.path) || basePath),
+        lastUpdate: moment(category.modified_gmt)
       })
-
-    const root = categories.find(_category => _category._url === baseUrl)
-    const rootId = root.id
-    root.setId(0)
-
-    categories.forEach(category => {
-      if (category.parentId === rootId) {
-        category.setParentId(0)
-      }
-      if (category.id !== 0) {
-        const parent = categories.find(_category => _category.id === category.parentId)
-        if (!parent) {
-          throw new Error(`Invalid data from categories endpoint: Page with id ${category.id} has no parent.`)
-        }
-        category.setParentUrl(parent.url)
-      }
     })
 
     return new CategoriesMapModel(categories)
